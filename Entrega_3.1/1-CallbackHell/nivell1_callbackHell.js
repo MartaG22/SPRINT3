9b67208777e9ee614d2f69db87756cdf68446471
rodolpop <martaxgarijo@gmail.com>
2022-07-18 12:10:03 +0200

    He afegit verificar si existeix la carpeta 'outbox' i si no existeix, la crea

--- Entrega_3.1/1-CallbackHell/nivell1_callbackHell.js
+++ Entrega_3.1/1-CallbackHell/nivell1_callbackHell.js
@@ -1,15 +1,3 @@
-// https://www.freecodecamp.org/espanol/news/tutorial-async-await-de-javascript-aprende-callbacks-promesas-y-async-await-en-js-haciendo-helados/
-// https://www.youtube.com/watch?v=WYVOvwTZ7Bo
-//! resolver callback hell escribir texto al reves
-// https://www.youtube.com/watch?v=IJNaoJokDco  BIEN EXPLICADO - CALLBACKS y ASYNAWAIT
-// https://www.youtube.com/watch?v=EVQR8TjjAWA  BIEN EXPLICADO - PROMESAS
-
-// https://www.youtube.com/watch?v=rKK1q7nFt7M  ASYNCAWAIT
-// https://lenguajejs.com/javascript/asincronia/promesas/
-// https://www.freecodecamp.org/espanol/news/promesas-en-javascript-es6/ ***********************************
-
-// https://www.tabnine.com/code/javascript/functions/fs/promises
-
 const {
   readdir,
   readFile,
@@ -28,9 +16,13 @@ const reverseText = str =>
     .reverse()
     .join("");
 
+const fs = require("fs");
 
 //mi código: ***********************************
 
+if (!fs.existsSync('./outbox')) {
+  fs.mkdirSync('./outbox');
+} else { console.log('Ja existeix la carpeta anomenada "outbox"') };
 
 readdir(inbox)
   .then(files => files.forEach(file => {
@@ -39,8 +31,6 @@ readdir(inbox)
       .then(data => writeFile(join(outbox, file), reverseText(data)))
       .catch(() => console.log("Error: File could not be saved!"))
       .then(() => console.log(`${file} was successfully saved in the outbox!`));
-
-    // if (error) return console.log("Error: Folder inaccessible");
 
   })).catch(() => console.log("Error: Folder inaccessible"));
 
