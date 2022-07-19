c7bae563bb3dd0d665a676bdaed4c75ec46de703
rodolpop <martaxgarijo@gmail.com>
2022-07-19 13:29:37 +0200

    He avançat l'exercici del Patró Oberver

--- Entrega_3.1/3-Observer/app.js
+++ Entrega_3.1/3-Observer/app.js
@@ -33,24 +33,118 @@ https://www.freecodecamp.org/news/how-to-code-your-own-event-emitter-in-node-js-
 https://www.tutorialspoint.com/nodejs/nodejs_event_emitter.htm*/
 
 
+
+
+
+
+
+
+
 // get the Emitter Module
 const events = require('events');
 
 //create an object of EventEmitter class by using above reference
-var em = new events.EventEmitter(); 
+var em = new events.EventEmitter();
 
-class Usuari {
-    constructor(nomUsuari, missatge) {
-        this.nomUsuari = nomUsuari;
-        this.missatge = missatge;
-    }
-}
 
 class Tema {
-    constructor(nomTema) {
-        this.nomTema = nomTema;
+    constructor() {
+        this.temes = [];
     }
-    notify(missatge) {
-        this.missatge = missatge;
+
+    subscribe(usuari) {
+        this.usuaris.push(usuari);
+    }
+
+    unsubscribe(usuari) {
+        this.usuari = this.usuari.filter(
+            (item) => item !== usuari
+        );
+    }
+
+    notify(event) {
+        this.usuari.forEach((item) => {
+            item.buzon.call(item, event);
+        });
     }
 }
+
+
+class Usuari {
+    constructor() {
+        this.usuaris = [];
+        // this.nomUsuari = nomUsuari;
+        // this.missatge = missatge;
+    }
+
+
+}
+
+const tema1 = new Tema();
+const tema2 = new Tema();
+
+
+const usuari1 = new Usuari(1);
+const usuari2 = new Usuari(2);
+const usuari3 = new Usuari(3);
+
+
+temes.subscribe(usuari1);
+tema.subscribe(subscriptor2);
+tema.notify("Nueva edicion");
+tema.subscribe(subscriptor3);
+tema.notify("Segunda edicion");
+tema.unsubscribe(subscriptor1);
+
+
+
+
+
+ //! BUENAAAA
+class Tema {
+    constructor() {
+        this.usuaris = [];
+    }
+
+
+    subscribe(subscriptor) {
+        this.subscriptors.push(subscriptor);
+    }
+
+
+    unsubscribe(subscriptor) {
+        this.subscriptors = this.subscriptors.filter(
+            (item) => item !== subscriptor
+        );
+    }
+
+
+    notify(event) {
+        this.subscriptors.forEach((item) => {
+            item.buzon.call(item, event);
+        });
+    }
+}
+
+
+
+
+class Usuari {
+    constructor(id) {
+        this.id = id;
+        console.log("Se ha creado el subscriptor #: " + id);
+    }
+    buzon(edicion) {
+        console.log(
+            "Subscriptor # " + this.id + " recibió una nueva edición: " + edicion
+        );
+    }
+}
+
+
+const tema = new Tema();
+console.log(tema.usuaris);
+
+const usuari1 = new Usuari(1);
+const usuari2 = new Usuari(2);
+const usuari3 = new Usuari(3);
