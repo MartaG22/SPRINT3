e3ef9257f1673fe657a0344b6e528a17ebb04e9b
rodolpop <martaxgarijo@gmail.com>
2022-07-21 13:06:29 +0200

    seguim amb middleware

--- Entrega_3.2/1-Middleware/app.js
+++ Entrega_3.2/1-Middleware/app.js
@@ -1,16 +1,12 @@
-// Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON.
-// Crea en un fitxer extern una classe que emmagatzemi middlewares (funcions).
-// Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands inicials en cadascuna de les operacions.
-// Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin mostrant per la consola les modificacions que es van fent als valors abans del resultat final.
-
-/* https://www.ticportal.es/glosario-tic/middleware
-https://www.iteramos.com/pregunta/16631/nodejs-que-es-res-y-req-en-expressjs
-
-https://www.youtube.com/watch?v=rA6Fgm7FL9E
-https://www.youtube.com/watch?v=j5UBvJhLA-k */
+// - Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON.
 
 
-// !DEVELOPERS TEAM:
+// https://www.youtube.com/watch?v=Y2FpY61h5Z8
 
-// https://www.youtube.com/watch?v=2mEdCSpfOHI
-// https://www.youtube.com/watch?v=B2ZJ2N1-iNI  - DAVID ESPANADERO
+const fs = require('fs');
+
+let data = fs.readFileSync('./personas.json');
+// console.log(data);
+let personas = JSON.parse(data);
+console.log(personas);
+console.log(typeof (personas));
