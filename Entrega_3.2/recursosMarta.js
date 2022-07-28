e446b58eb5ffde95dc217eb573b2f9243ca94eab
rodolpop <martaxgarijo@gmail.com>
2022-07-28 13:19:41 +0200

    Recursos

---
+++ Entrega_3.2/recursosMarta.js
@@ -0,0 +1,18 @@
+// https://www.freecodecamp.org/espanol/news/tutorial-async-await-de-javascript-aprende-callbacks-promesas-y-async-await-en-js-haciendo-helados/
+// https://www.youtube.com/watch?v=WYVOvwTZ7Bo
+//! resolver callback hell escribir texto al reves
+// https://www.youtube.com/watch?v=IJNaoJokDco  BIEN EXPLICADO - CALLBACKS y ASYNAWAIT
+// https://www.youtube.com/watch?v=EVQR8TjjAWA  BIEN EXPLICADO - PROMESAS
+
+// https://www.youtube.com/watch?v=rKK1q7nFt7M  ASYNCAWAIT
+// https://lenguajejs.com/javascript/asincronia/promesas/
+// https://www.freecodecamp.org/espanol/news/promesas-en-javascript-es6/ ***********************************
+
+// https://www.tabnine.com/code/javascript/functions/fs/promises
+
+
+
+//!   PATRONES
+// youtube.com/watch?v=Z7Z-PJEIEs  singletton
+
+// https://www.freecodecamp.org/espanol/news/40-proyectos-de-javascript-para-principiantes-ideas-faciles-para-empezar-a-codificar-en-js/
