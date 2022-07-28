9cbe7444205aea02e5b4c1da7a5fc6e0073ff1e2
rodolpop <martaxgarijo@gmail.com>
2022-07-28 13:17:03 +0200

    Modificant Middleware

--- Entrega_3.2/1-Middleware/app.js
+++ Entrega_3.2/1-Middleware/app.js
@@ -5,20 +5,48 @@ const Middleware = require('./middleware.js');
 let data = fs.readFileSync('./nombres.json');
 let nombres = JSON.parse(data);
 console.log(nombres);
-
+// console.log(nombres.num1);
 
 const calculadora = new Calculadora;
 const middleware = new Middleware;
 
-middleware.use(calculadora.sumar)
+
+//!OMAR
+middleware.use((data, next) => {
+    data.num1 = data.num1 ** 2;
+    data.num2 = data.num2 ** 2;
+    
+    console.log( "sdaf",data.num1, data.num2);
+    next();
+});
+
+//! Els següents estan malament:
+middleware.use(calculadora.restar);
+middleware.use(calculadora.multiplicar);
 
 
 
-middleware.use(function (nombres, next) {
-    console.log(`El resultat de la suma és ${calculadora.sumar(nombres)}`);
+// middleware.use(funcions(nombres, next) {
+//     console.log(`El resultat de la suma és ${funcions(nombres)}`);
 
-}) 
+// })
+
+
+
+// let quadratNum1 = nombres.num1 * nombres.num2;
+// let quadratNum2 = nombres.num2 * nombres.num2;
+// // console.log(nombres.num1,quadratNum1)
+// let quadratNombres = [quadratNum1, quadratNum2];
+// console.log(`Els nombres al quadrat: ${quadratNombres}`);
+
+middleware.run(calculadora.sumar, nombres)
+middleware.run(calculadora.restar, nombres)
+middleware.run(calculadora.multiplicar,nombres )
+
+
 
 console.log(`El resultat de la suma és ${calculadora.sumar(nombres)}`);
 console.log(`El resultat de la resta és ${calculadora.restar(nombres)}`);
 console.log(`El resultat de la multiplicació és ${calculadora.multiplicar(nombres)}`);
+console.log(middleware.funcio);
+middleware.use(nombres);
--- Entrega_3.2/1-Middleware/middleware.js
+++ Entrega_3.2/1-Middleware/middleware.js
@@ -2,7 +2,7 @@
 
 
 
-	
+
 // Vale, Marta, t'estàs complicant MOLT!
 // Hauries de tenir dues classes: la calculadora (amb les funcions matemàtiques) i la de middlewares (amb el gestor de MW).
 // Primer instanciaras la classe calculadora i la de middlewares, afegiràs els middlewares a la classe (amb un mètode de l'estil "middlewares.use(funció)" )
@@ -14,14 +14,23 @@
 
 
 class Middleware {
-    constructor(funcions) {
-        this.funcions = [];
+    constructor() {
+        this.funcio = [];
+        this.data = [];
     }
-    pasarNumeros(numeros){
-        this.numeros = [];
-    }
+    // pasarNumeros(numeros){
+    //     this.numeros = [];
+    // }
     use(fn) {
-        this.funcions.push(fn);
+        this.funcio.push(fn);
+    }
+    run(funcio, data) {
+        // let result = funcio(data)
+        // console.log(result)
+        // console.log(`El resultat de la suma és ${result}`);
+        // console.log(`El resultat de la resta és ${result}`);
+        // console.log(`El resultat de la multiplicació és ${result}`);
+
     }
 }
 
