57c8fa6e9dd29de03b1c51309b94362758b77154
M G <martaxgarijo@gmail.com>
2022-08-29 21:49:54 +0200

    Finalitzat exercici Decorator Pattern

--- Entrega_3.2/2-Decorator/app.js
+++ Entrega_3.2/2-Decorator/app.js
@@ -3,15 +3,13 @@
 // Crea una petita aplicació que calculi el cost d'uns quants Articles en euros a partir de les seves divises inicials, aplicant diferents conversions que usin el Decorator del punt anterior.
 
 
-
 // https://mauriciogc.medium.com/javascript-patrones-de-dise%C3%B1o-en-js-estructurales-parte-ii-79b87905cefe
 // https://blog.logrocket.com/understanding-javascript-decorators/#:~:text=A%20decorator%20(also%20known%20as,treated%20as%20first%2Dclass%20citizens.
 
 const Producte = require("./producte.js");
-const conversioDivisa = require("./decorator.js")
+const conversioDivisa = require("./decorator.js");
+const { receiveMessageOnPort } = require("worker_threads");
 
-
-// let producte = new Producte();
 
 // Creem productes:
 const producte1 = new Producte('Portàtil Acer Swift XSFX14', 1099, "USD");
@@ -23,24 +21,6 @@ const producte6 = new Producte('Portàtil CHUWI HeroBook Pro 14.1"', 1826.95, "C
 const producte7 = new Producte('Portàtil HP Omen 16 Gaming 16.1" i7', 1749.99, "CAD");
 const producte8 = new Producte('Portàtil LG Gram 15Z95P Laptop 15.6"', 71386.11, "RUB"); 
 
+const arrayProductes = [producte1, producte2, producte3, producte4, producte5, producte6, producte7, producte8];
 
-conversioDivisa(producte1);
-conversioDivisa(producte2);
-conversioDivisa(producte3);
-conversioDivisa(producte4);
-conversioDivisa(producte5);
-conversioDivisa(producte6);
-conversioDivisa(producte7);
-conversioDivisa(producte8);
-
-
-// function showConversion(){
-//     for (let i=1; i<=8; i++){
-//         let showProducte = "producte"+i;
-//         console.log("producte:",showProducte)
-//         conversioDivisa(showProducte)
-
-//     }
-// };
-
-// showConversion(showProducte)
+arrayProductes.forEach(producte => conversioDivisa(producte))
--- Entrega_3.2/2-Decorator/decorator.js
+++ Entrega_3.2/2-Decorator/decorator.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const readDivises = fs.readFileSync("./divises.json");
 const taulaFactorsConversio = JSON.parse(readDivises);
 
-// console.log("taula", taulaFactorsConversio)
-
 
 function conversioDivisa(producte) {
     let divisaInicial = "";
@@ -13,31 +11,13 @@ function conversioDivisa(producte) {
 
     divisaInicial = producte.divisa;
     conversioDivisaInical = divisaInicial + "_EUR";
-    // console.log("divisa inicial:", conversioDivisaInical);
-    // // let coeficientConversio = data.find(divisaInicial);
-    // // console.log("coeficient:", coeficientConversio)
     const preuDivisaInicial = producte.preu;
-    // console.log("preu:", preuDivisaInicial, ", divisa inicial:", divisaInicial)
-    // console.log("producte", producte);
-
 
     factorConversio = taulaFactorsConversio[conversioDivisaInical];
-    // // let factorConversio =     JSON.parse(conversioDivisaInical)
-
-    // // taulaFactorsConversio
-
-    // console.log("taula", taulaFactorsConversio)
-    // console.log("factor:", factorConversio)
-    // //Falta ver como encontrar el valor en el JSON para acabar de hacer la conversión!!!
-    // console.log("factor conversió:", factorConversio);
     conversioPreuEUR = (producte.preu * factorConversio).toFixed(2);
-    // console.log("conversio preu:", conversioPreuEUR)
-    // number.toLocaleString()
+
     console.log(`Coversió de ${divisaInicial} a EUR:`);
-
     console.log(`\n El producte: ${producte.nomProducte} amb un preu de ${preuDivisaInicial} ${divisaInicial} costarà ${conversioPreuEUR} EUR\n`);
-
-
 };
 
 
