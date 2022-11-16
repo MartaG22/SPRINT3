eaefec05df5c591103b7dbd0a639263e643eeaf2
Marta <martaxgarijo@gmail.com>
2022-11-16 09:57:20 +0100

    arreglant conversió de divises

--- Entrega_3.2/2-Decorator/decorator.js
+++ Entrega_3.2/2-Decorator/decorator.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const readDivises = fs.readFileSync("./divises.json");
 const taulaFactorsConversio = JSON.parse(readDivises);
-const ProducteDecorat = require('./producteDecorat.js')
+// const ProducteDecorat = require('./producteDecorat.js')
 
 function conversioDivisa(producte) {
     console.log("producte en DECORATOR:", producte)
-    let divisaInicial = "";
-    let conversioDivisaInical = "";
-    let factorConversio = 0;
+    // let divisaInicial = "";
+    // let conversioDivisaInical = "";
+    // let factorConversio = 0;
     let conversioPreuEUR = 0;
 
-    divisaInicial = producte.divisa;
-    conversioDivisaInical = divisaInicial + "_EUR";
+    let divisaInicial = producte.divisa;
+    // let conversioDivisaInical = divisaInicial + "_EUR";
     const preuDivisaInicial = producte.preu;
 
-    factorConversio = taulaFactorsConversio[conversioDivisaInical];
+    // factorConversio = taulaFactorsConversio[conversioDivisaInical];
+    let factorConversio = taulaFactorsConversio[divisaInicial + "_EUR"];
     conversioPreuEUR = (producte.preu * factorConversio).toFixed(2);
-    new ProducteDecorat(conversioPreuEUR);
+    // new ProducteDecorat(conversioPreuEUR);
+    producte.preuEuros = conversioPreuEUR;
+    return producte;
+
+
+
 
     console.log(`Coversió de ${divisaInicial} a EUR:`);
     console.log(`\n El producte: ${producte.nomProducte} amb un preu de ${preuDivisaInicial} ${divisaInicial} costarà ${conversioPreuEUR} EUR\n`);
